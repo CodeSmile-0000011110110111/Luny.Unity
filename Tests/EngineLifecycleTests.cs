6960a477392492e557e3fa22b4da6473a2079149
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-18 20:04:31 +0100

    update test scene

--- Tests/EngineLifecycleTests.cs
+++ Tests/EngineLifecycleTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Luny.Unity.Tests
@@ -11,8 +12,10 @@ namespace Luny.Unity.Tests
 		{
 			// Mock lifecycle auto-instantiates and is already running at this point
 			// If we get here without exceptions from the mock, the test passes
-			yield return null;
-			Assert.Pass();
+			yield return new WaitForEndOfFrame();
+
+			// TODO: get observer and test its fields
+			Assert.Fail();
 		}
 	}
 }
