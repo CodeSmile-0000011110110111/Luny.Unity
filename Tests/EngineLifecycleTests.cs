da1b13a6f01fd78aa030095820cfef1d39822bfa
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-18 20:39:52 +0100

    disable unit test

--- Tests/EngineLifecycleTests.cs
+++ Tests/EngineLifecycleTests.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+using Luny.Tests;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -7,15 +7,16 @@ namespace Luny.Unity.Tests
 {
 	public sealed class EngineLifecycleTests
 	{
-		[UnityTest]
+		/*[UnityTest]
 		public IEnumerator EngineLifecycleExpectedEventOrder()
 		{
 			// Mock lifecycle auto-instantiates and is already running at this point
 			// If we get here without exceptions from the mock, the test passes
 			yield return new WaitForEndOfFrame();
 
-			// TODO: get observer and test its fields
-			Assert.Fail();
-		}
+			// get observer and test its fields
+			var mockObserver = LunyEngine.Instance.GetObserver<EngineLifecycleExpectedEventOrderMock>();
+			mockObserver.PostShutdownVerification();
+		}*/
 	}
 }
