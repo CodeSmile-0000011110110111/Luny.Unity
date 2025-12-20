aea5fa155dc8bf499fa8e14f912b484106a79652
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-20 13:29:03 +0100

    add time service

---
+++ Providers/UnityTimeServiceProvider.cs
@@ -0,0 +1,17 @@
+using Luny.Providers;
+using System;
+using UnityEngine;
+
+namespace Luny.Unity.Providers
+{
+	/// <summary>
+	/// Unity implementation of time service provider.
+	/// Uses Unity's Time.frameCount and Time.realtimeSinceStartupAsDouble for cross-platform consistency.
+	/// </summary>
+	public sealed class UnityTimeServiceProvider : ITimeServiceProvider
+	{
+		public Int64 FrameCount => Time.frameCount;
+
+		public Double ElapsedSeconds => Time.realtimeSinceStartupAsDouble;
+	}
+}
