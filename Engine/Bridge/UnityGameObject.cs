d03828396f949995c71dc813afa481538ee3ef9c
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-15 19:14:39 +0100

    testing recursive commit

--- Engine/Bridge/UnityGameObject.cs
+++ Engine/Bridge/UnityGameObject.cs
@@ -13,13 +13,10 @@ namespace Luny.Unity.Engine.Bridge
 		private Renderer _renderer;
 		private Renderer Renderer => _renderer != null ? _renderer : GO.TryGetComponent(out _renderer) ? _renderer : null;
 		private GameObject GO => Cast<GameObject>();
-
 		private static Boolean IsNativeObjectVisible(GameObject gameObject) =>
 			gameObject.TryGetComponent<Renderer>(out var renderer) && renderer.enabled;
-
 		public UnityGameObject(GameObject gameObject)
 			: base(gameObject, gameObject.GetEntityId(), gameObject.activeSelf, IsNativeObjectVisible(gameObject)) {}
-
 		protected override void DestroyNativeObject() => Object.Destroy(GO);
 		protected override Boolean IsNativeObjectValid() => GO != null;
 		protected override String GetNativeObjectName() => GO.name;
@@ -28,17 +25,7 @@ namespace Luny.Unity.Engine.Bridge
 		protected override Boolean GetNativeObjectEnabled() => GO.activeSelf;
 		protected override void SetNativeObjectEnabled() => GO.SetActive(true);
 		protected override void SetNativeObjectDisabled() => GO.SetActive(false);
-
-		protected override void SetNativeObjectVisible()
-		{
-			if (Renderer != null)
-				Renderer.enabled = true;
-		}
-
-		protected override void SetNativeObjectInvisible()
-		{
-			if (Renderer != null)
-				Renderer.enabled = false;
-		}
+		protected override void SetNativeObjectVisible() { if (Renderer != null) Renderer.enabled = true; }
+		protected override void SetNativeObjectInvisible() { if (Renderer != null) Renderer.enabled = false; }
 	}
 }
