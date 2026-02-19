719f846746a817d8aab8098b25983d17566a19ff
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-19 12:09:19 +0100

    code cleanup

--- Engine/Bridge/LunyVectorExt.cs
+++ Engine/Bridge/LunyVectorExt.cs
@@ -1,16 +1,17 @@
 using Luny.Engine.Bridge;
+using UnityEngine;
 
 namespace Luny.Unity.Engine.Bridge
 {
 	public static class LunyVectorExt
 	{
-		public static UnityEngine.Vector3 ToUnity(this LunyVector3 v) => new(v.X, v.Y, v.Z);
-		public static LunyVector3 ToLuny(this UnityEngine.Vector3 v) => new(v.x, v.y, v.z);
+		public static Vector3 ToUnity(this LunyVector3 v) => new(v.X, v.Y, v.Z);
+		public static LunyVector3 ToLuny(this Vector3 v) => new(v.x, v.y, v.z);
 
-		public static UnityEngine.Vector2 ToUnity(this LunyVector2 v) => new(v.X, v.Y);
-		public static LunyVector2 ToLuny(this UnityEngine.Vector2 v) => new(v.x, v.y);
+		public static Vector2 ToUnity(this LunyVector2 v) => new(v.X, v.Y);
+		public static LunyVector2 ToLuny(this Vector2 v) => new(v.x, v.y);
 
-		public static UnityEngine.Quaternion ToUnity(this LunyQuaternion q) => new(q.X, q.Y, q.Z, q.W);
-		public static LunyQuaternion ToLuny(this UnityEngine.Quaternion q) => new(q.x, q.y, q.z, q.w);
+		public static Quaternion ToUnity(this LunyQuaternion q) => new(q.X, q.Y, q.Z, q.W);
+		public static LunyQuaternion ToLuny(this Quaternion q) => new(q.x, q.y, q.z, q.w);
 	}
 }
