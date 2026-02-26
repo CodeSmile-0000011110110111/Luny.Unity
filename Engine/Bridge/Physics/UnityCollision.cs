7aa1efdfe3dc7aa22b0883b0542269e9aac14b34
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-26 22:24:00 +0100

    On.Collision cooldown works

---
+++ Engine/Bridge/Physics/UnityCollision.cs
@@ -0,0 +1,17 @@
+﻿using Luny.Engine.Bridge.Physics;
+using System;
+using UnityEngine;
+
+namespace Luny.Unity.Engine.Bridge.Physics
+{
+	public sealed class UnityCollision : LunyCollision
+	{
+		private Collision Collision => (Collision)NativeObject;
+
+		public override String Tag => Collision.gameObject.tag;
+		public override String Name => Collision.gameObject.name;
+		public override String LayerName => LayerMask.LayerToName(Layer);
+		public override Int32 Layer => Collision.gameObject.layer;
+		public override Boolean HasComponent(Type type) => Collision.gameObject.TryGetComponent(type, out var _);
+	}
+}
