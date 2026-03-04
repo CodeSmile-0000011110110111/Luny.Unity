be8d7fce4afe7cef20273aba2687b4ab315b32d8
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-04 11:28:00 +0100

    fix missingref with collider

--- Engine/Bridge/Physics/UnityCollision.cs
+++ Engine/Bridge/Physics/UnityCollision.cs
@@ -1,28 +1,25 @@
 ﻿using Luny.Engine.Bridge.Physics;
 using System;
 using UnityEngine;
+using Object = System.Object;
 
 namespace Luny.Unity.Engine.Bridge.Physics
 {
 	public sealed class UnityCollision : LunyCollision
 	{
-		private LunyCollider _collider;
+		public Collision Collision => (Collision)NativeCollision;
 
-		private Collision Collision => (Collision)NativeObject;
-		private Boolean IsValid => _nativeObject != null;
-
-		public override LunyCollider Collider => _collider ??= IsValid ? new UnityCollider(Collision.collider) : null;
+		public override LunyCollider Collider { get; } = new UnityCollider();
 		public override String Tag => IsValid ? Collision.gameObject.tag : null;
 		public override String Name => IsValid ? Collision.gameObject.name : null;
 		public override String LayerName => IsValid ? LayerMask.LayerToName(Collision.gameObject.layer) : null;
 		public override Int32 Layer => IsValid ? Collision.gameObject.layer : 0;
 
-		internal UnityCollision() {}
-
-		public UnityCollision(Collision collision)
-			: base(collision) {}
-
-		internal void SetNativeObject(Collision nativeObject) => _nativeObject = nativeObject;
+		public override void SetNativeCollision(Object nativeCollision)
+		{
+			base.SetNativeCollision(nativeCollision);
+			Collider.SetNativeCollider(IsValid ? Collision.collider : null);
+		}
 
 		public override Boolean HasComponent(Type type) => IsValid && Collision.gameObject.TryGetComponent(type, out var _);
 	}
