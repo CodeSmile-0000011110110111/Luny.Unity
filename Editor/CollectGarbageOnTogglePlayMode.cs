fa77722cd334ffed655ff1d91269218314fccf83
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-26 21:34:50 +0100

    block diagnostics: fix display of logical operators and other display tweaks

---
+++ Editor/CollectGarbageOnTogglePlayMode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Luny.UnityEditor
+{
+	internal static class CollectGarbageOnTogglePlayMode
+	{
+		[RuntimeInitializeOnLoadMethod]
+		private static void InitializeOnLoad()
+		{
+			EditorApplication.playModeStateChanged -= OnPlaymodeStateChanged;
+			EditorApplication.playModeStateChanged += OnPlaymodeStateChanged;
+		}
+
+		private static void OnPlaymodeStateChanged(PlayModeStateChange mode)
+		{
+			if (mode == PlayModeStateChange.EnteredPlayMode || mode == PlayModeStateChange.EnteredEditMode)
+				CollectAndReport();
+		}
+
+		private static void CollectAndReport()
+		{
+			var memoryBefore = GC.GetTotalMemory(false);
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			var memoryAfter = GC.GetTotalMemory(true);
+			var reclaimed = memoryBefore - memoryAfter;
+			LunyLogger.LogInfo($"{reclaimed / 1024.0 / 1024.0:F1} MB garbage collected", nameof(CollectGarbageOnTogglePlayMode));
+		}
+	}
+}
